c957d846d3e2bcf6865523f529dc3150d334c25a
Manuel Bento <65732773+manuelbento19@users.noreply.github.com>
2021-12-02 10:19:49 +0000

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const route = express.Router();
+const fs = require('fs')
+const path = require('path')
+
+const controllersPath = path.join(__dirname,'../app/controllers');
 
 
-require(`../app/controllers/usersController.js`)(route);
-//require(`./../app/controllers/produtosController`)(route);
-//require(`./../app/controllers/devsController`)(route);
+//Basicamente, ele percorre e filtra tudo quanto é ficheiro .js na pasta controllers... tudo que precisas de fazer é "criares os controllers segundo o controller inicial" 
+fs
+.readdirSync(controllersPath)
+.filter(file => {
+    return (file.indexOf('.') !== 0) && (file.slice(-3) === '.js');
+})
+.forEach(file => {
+      require(path.join(controllersPath, file))(route);
+});
 
 module.exports = route;
 
